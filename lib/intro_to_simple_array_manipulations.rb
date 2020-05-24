6b5942ebea4224d47255f34e3f30d46900905f5c
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-24 06:31:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,4 +10,14 @@ def using_pop(array)
   array.pop 
 end
 
-def 
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
